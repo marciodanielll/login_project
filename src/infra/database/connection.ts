271d1d8d1ca46f8522152338b66f96bace9e7158
Márcio Daniel <marcio.daniel@msn.com>
2023-03-04 19:33:14 -0300

    feat: add conncetion with database

---
+++ src/infra/database/connection.ts
@@ -0,0 +1,21 @@
+import mysql2, { Pool, PoolConnection } from 'mysql2/promise';
+import config from '../../../configs/env.vars';
+
+const { database: { name: _, ...others } } = config;
+
+let db: PoolConnection;
+
+const getConnection = async (): Promise<PoolConnection> => {
+  if (db) {
+    return db;
+  }
+
+  const pool: Pool = mysql2.createPool(others);
+  const connection: PoolConnection = await pool.getConnection();
+
+  db = connection;
+
+  return connection;
+};
+
+export default getConnection;
