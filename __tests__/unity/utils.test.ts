8cc9f6d7363bf2ef8e64b17ce9bd50b21f79538b
Márcio Daniel <marcio.daniel@msn.com>
2023-03-04 17:27:37 -0300

    test: add tests for utils.ts"

---
+++ __tests__/unity/utils.test.ts
@@ -0,0 +1,22 @@
+import chai from 'chai';
+import { showDataType } from '../utils';
+
+const { expect } = chai;
+
+describe('Utils', () => {
+  it('should return a type array', () => {
+    expect(showDataType([])).to.be.equals('array');
+  });
+  it('should return a type object', () => {
+    expect(showDataType({})).to.be.equals('object');
+  });
+  it('should return a type number', () => {
+    expect(showDataType(1)).to.be.equals('number');
+  });
+  it('should return a type string', () => {
+    expect(showDataType('1')).to.be.equals('string');
+  });
+  it('should return a type boolean', () => {
+    expect(showDataType(true)).to.be.equals('boolean');
+  });
+});
