3d8343a9a72e4aa9961e2064699acaba43560907
Márcio Daniel <marcio.daniel@msn.com>
2023-03-04 21:02:54 -0300

    refactor: structure and readability

--- src/infra/database/seeds.ts
+++ src/infra/database/seeds.ts
@@ -1,20 +1,21 @@
 import { PoolConnection } from 'mysql2/promise';
 import getConnection from './connection';
 
-const { log } = console;
+const { log, time, timeEnd } = console;
 
 const seeds = async (): Promise<void> => {
+  time('\x1b[32mSeeds query successfully\x1b[0m');
   const db: PoolConnection = await getConnection();
   db.beginTransaction();
 
   try {
     await db.query(`
-      INSERT INTO user (first_name, last_name, email, password)
+      INSERT INTO login.user (first_name, last_name, email, password)
       VALUES ('Márcio', 'Daniel', 'marcio@daniel', 'mudar123');
     `);
 
     await db.query(`
-      INSERT INTO state (name, uf) VALUES
+      INSERT INTO login.state (name, uf) VALUES
       ('Acre', 'AC'), ('Alagoas', 'AL'), ('Amapá', 'AP'), ('Amazonas', 'AM'),
       ('Bahia', 'BA'), ('Ceará', 'CE'), ('Distrito Federal', 'DF'),
       ('Espírito Santo', 'ES'), ('Goiás', 'GO'), ('Maranhão', 'MA'),
@@ -26,31 +27,32 @@ const seeds = async (): Promise<void> => {
     `);
 
     await db.query(`
-      INSERT INTO city (name, state_id)
+      INSERT INTO login.city (name, state_id)
       VALUES ('São Paulo', 1);
     `);
 
     await db.query(`
-      INSERT INTO address (street, number, complement, neighborhood, zip_code, city_id)
+      INSERT INTO login.address (street, number, complement, neighborhood, zip_code, city_id)
       VALUES ('Av. Paulista', '1000', 'Apto 123', 'Bela Vista', '01310-100', 1);
     `);
 
     await db.query(`
-      INSERT INTO user_address (user_id, address_id)
+      INSERT INTO login.user_address (user_id, address_id)
       VALUES (1, 1);
     `);
 
-    db.commit();
-    process.exit(0);
+    await db.commit();
+    timeEnd('\x1b[32mSeeds query successfully\x1b[0m');
   } catch (err) {
     log(err);
 
-    db.rollback();
-    process.exit(1);
+    await db.rollback();
   } finally {
     db.release();
-    process.exit(0);
+    process.exit();
   }
 };
 
 export default seeds;
+
+seeds();
