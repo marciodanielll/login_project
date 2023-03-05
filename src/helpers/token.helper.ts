1fe173b33e99d05854cc7762e0de725cf2b82502
Márcio Daniel <marcio.daniel@msn.com>
2023-03-05 17:46:13 -0300

    feat: add Talker.Helper

---
+++ src/helpers/token.helper.ts
@@ -0,0 +1,39 @@
+import jwt, { SignOptions, VerifyOptions } from 'jsonwebtoken';
+import config from '../../configs/env.vars';
+
+const { security } = config;
+
+export interface TokenPayload {
+  userId: string;
+  email: string;
+}
+
+export default class TokenHelper {
+  public readonly secret: string;
+
+  public readonly expiresIn: string;
+
+  public readonly jwt: typeof jwt;
+
+  constructor() {
+    this.secret = security.secret;
+    this.expiresIn = security.expiresIn;
+    this.jwt = jwt;
+  }
+
+  create(payload: TokenPayload): string {
+    const token = this.jwt.sign(payload, this.secret, { expiresIn: this.expiresIn } as SignOptions);
+    return token;
+  }
+
+  validate(token: string): TokenPayload | null {
+    try {
+      const decodedToken = this.jwt
+        .verify(token, this.secret, { ignoreExpiration: false } as VerifyOptions);
+
+      return decodedToken as TokenPayload;
+    } catch (err) {
+      return null;
+    }
+  }
+}
