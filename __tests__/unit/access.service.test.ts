7e28f35dbe1a88238881f40248f8e0336352cb2d
Márcio Daniel <marcio.daniel@msn.com>
2023-03-12 17:03:42 -0700

    test: write test for singIn method AccessService

---
+++ __tests__/unit/access.service.test.ts
@@ -0,0 +1,43 @@
+import { expect } from 'chai';
+import sinon from 'sinon';
+import AccessService from '../../src/services/access.service';
+
+const userMoc = {
+  id: 1,
+  firstName: 'Márcio',
+  lastName: 'Daniel',
+  email: 'dan@gmail.com',
+  password: '12345678',
+};
+
+describe('AccessService', () => {
+  let sut: AccessService;
+
+  before(() => {
+    sut = new AccessService();
+  });
+  describe('signIn', () => {
+    describe('in success case', () => {
+      it('should return a token', async () => {
+        const getUserByEmailStub = sinon.stub().resolves(userMoc);
+        const compareHashStub = sinon.stub().resolves(true);
+        const createTokenStub = sinon.stub().returns('token');
+
+        sinon.replace(sut, 'userModel', {
+          getUserByEmail: getUserByEmailStub,
+        });
+
+        sinon.replace(sut, 'cryptoHelper', {
+          compareHash: compareHashStub,
+        });
+
+        sinon.replace(sut, 'tokenHelper', {
+          create: createTokenStub,
+        });
+
+        const token = await sut.signIn(userMoc.email, userMoc.password);
+        expect(token).to.be.a('string');
+      });
+    });
+  });
+});
