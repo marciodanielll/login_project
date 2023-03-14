c9444c187cf85405bc92a37fbc91da3f8e4996cc
Márcio Daniel <marcio.daniel@msn.com>
2023-03-13 21:14:51 -0300

    feat: add validatons for Accesss entity

---
+++ src/interface/validations/access.validation.ts
@@ -0,0 +1,30 @@
+import Joi from 'joi';
+import { IUserForCreate, IUserForLogin } from '../../types/user.types';
+
+export default class AccessValidation {
+  private static schemaSingUp = Joi.object({
+    firstName: Joi.string().required(),
+    lastName: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).max(15).required(),
+  });
+
+  private static schemaSingIn = Joi.object({
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).max(15).required(),
+  });
+
+  static singUp(user: IUserForCreate): string | null {
+    const { schemaSingUp } = this;
+    const { error } = schemaSingUp.validate(user);
+    if (error) return error.details[0].message;
+    return null;
+  }
+
+  static singIn(user: IUserForLogin): string | null {
+    const { schemaSingIn } = this;
+    const { error } = schemaSingIn.validate(user);
+    if (error) return error.details[0].message;
+    return null;
+  }
+}
